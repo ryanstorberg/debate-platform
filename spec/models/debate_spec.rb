7568134bf5d9bd41c9859a5a3eedfe1bcf275ecc
Alan Florendo & Justin Lee <pair+alanflorendo.leeju@devbootcamp.com>
2014-08-29 16:12:01 -0400

    correct misspelling in Debate(s)

--- spec/models/debate_spec.rb
+++ spec/models/debate_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Debate do
   it {should validate_presence_of(:title)}
   it {should validate_presence_of(:body)}
-  it {should belong_to(:user)}
+  it {should belong_to(:author)}
   it {should have_many(:comments)}
   it {should have_many(:votes)}
   it {should belong_to(:topic)}
