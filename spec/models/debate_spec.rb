eb9892a42015e79b0e7bfc30da415b0bd0317d91
Alan Florendo & Justin Lee <pair+alanflorendo.leeju@devbootcamp.com>
2014-08-29 14:45:48 -0400

    Add RSpec title and body validations to Debate model

--- spec/models/debate_spec.rb
+++ spec/models/debate_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe Debate do
+  it {should validate_presence_of(:title)}
+  it {should validate_presence_of(:body)}
   it {should belong_to(:user)}
   it {should have_many(:comments)}
   it {should have_many(:votes)}
