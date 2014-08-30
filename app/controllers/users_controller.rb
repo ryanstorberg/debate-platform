93dcec8f4c5b582479fc07b4d842b5ef3015e876
Ryan Storberg <ryan@storberg.net>
2014-08-30 17:44:11 -0400

    update signup form; change root directory; fix bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,9 @@ class UsersController < ApplicationController
   end
 
   private
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
 end
