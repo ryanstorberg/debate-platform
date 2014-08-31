19a4dbb2470fb72181b9da804aa835294bb82ef2
Chase Blokker <chaseblokker@yahoo.com>
2014-08-31 16:22:27 -0400

    made some style changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,26 @@ require 'faker'
   )
 end
 
-10.times do
-  Topic.create(name: Faker::Lorem.word)
+topics = [
+  "Abortion", "ACLU", "Alternative Energy", "Animal Testing",
+  "Climate Change", "Church and State", "College Education",
+  "Gun Control", "Cuba Embargo", "Drones", "Gay Marriage",
+  "Gold Standard", "Illegal Immigration",
+  "Israeli Palestinian Conflict", "Medical Marijuana",
+  "Obamacare / Healthcare", "Social Security",
+  "Standardized Tests", "Teacher Tenure", "Vaccines for Kids",
+  "U.S Foreign Policy", "Poverty"
+]
+
+topics.each do |topic|
+  Topic.create(name: topic)
 end
 
 20.times do
   Debate.create(
     title: Faker::Company.catch_phrase,
     body: Faker::Lorem.paragraph(rand(5..50)),
-    topic_id: rand(1..10),
+    topic_id: rand(1..topics.length),
     user_id: rand(1..10)
     # votes_for: rand(1..50),
     # votes_against: rand(1..50))
